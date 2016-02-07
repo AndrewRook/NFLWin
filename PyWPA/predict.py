53613bb89229cafca7c7635dfcd16599c99a28c7
Andrew Schechtman-Rook <rook166@gmail.com>
2016-02-06 22:06:19 -0500

    starting on WPA computation

--- PyWPA/predict.py
+++ PyWPA/predict.py
@@ -34,14 +34,14 @@ def load_model():
 
     return model
 
-def compute_wp(model, data):
+def compute_wp(model, play_data):
     '''
     Compute the Win Percentage for a given situation.
 
     Arguments:
     model: A dictionary containing model information (at least the 'fit_model'
         and 'bootstrapped_models' keys) loaded in from load_model().
-    data: Either a dictionary or Pandas DataFrame with at least the
+    play_data: Either a dictionary or Pandas Play_dataFrame with at least the
         following keys/columns:
         quarter: The quarter the game is in (1,2,3,4, or 5 for anything in OT).
         time_remaining: Seconds counting down from the start of the quarter
@@ -55,26 +55,44 @@ def compute_wp(model, data):
             (1 is your one-yard line, 99 is 1 yard from a touchdown).
 
     Returns:
-    Prediction: A dictionary/DataFrame with the following keys/columns:
+    prediction: A dictionary/Play_dataFrame with the following keys/columns:
         WP: The predicted win percentage for the play.
         WP_error: The predicted uncertainty in the WP estimate based on
             sampling error.
     '''
-    rescaled_data = rescale_data(data)
+    rescaled_play_data = rescale_play_data(play_data)
     try:
-        #Start by assuming the data is a dictionary:
-        features = rescaled_data[cf.DATA_COLUMNS[:-1]].values
+        #Start by assuming the play_data is a dictionary:
+        features = rescaled_play_data[cf.DATA_COLUMNS[:-1]].values
+        prediction = {}
     except TypeError:
         feature_list = []
         for key in cf.DATA_COLUMNS[:-1]:
-            feature_list.append(rescaled_data[key])
+            feature_list.append(rescaled_play_data[key])
         features = np.array(feature_list).reshape(1,-1)
+        prediction = pd.DataFrame(index=np.arange(features.shape[0]))
 
     win_prob = model['fit_model'].predict_proba(features)[:,1]
     win_prob_errors = np.std([bootstrapped_model.predict_proba(features)[:,1] \
                        for bootstrapped_model in model['bootstrapped_models']], axis=0, ddof=1)
     print(win_prob)
     print(win_prob_errors)
+    prediction['win_prob'] = win_prob
+    prediction['win_prob_errors'] = win_prob_errors
+
+    return prediction
+
+def compute_wpa(model, before_play_data, after_play_data):
+    '''
+    A simple wrapper around compute_wp to compute the win probability added.
+
+    model: see the docstring for the 'model' argument of compute_wp.
+    before_play_data: see the docstring for the 'play_data' argument of compute_wp.
+    after_play_data: Same rules as for before_play_data, except containing the game
+        state after the play(s) end. 
+    '''
+    
+    
 
 if __name__ == "__main__":
     import time
