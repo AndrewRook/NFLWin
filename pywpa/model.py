e5415c05f5d39bce9cc44a4cac882cf98db95c07
Andrew Schechtman-Rook <rook166@gmail.com>
2016-05-31 21:47:31 -0400

    removing some cruft

--- pywpa/model.py
+++ pywpa/model.py
@@ -40,31 +40,17 @@ class WPModel(object):
         The name of the column containing the abbreviation for the team currently on offense.
     home_team_colname : string (default="home_team")
         The name of the column containing the abbreviation for the home team.
-    copy : boolean (default=``True``)
-        Whether or not to copy data at each step in the modeling pipeline. Setting this to ``False``
-        can speed up execution time but will also overwrite your input data.
-    model_class : A valid Scikit-Learn classifier (default=``sklearn.linear_model.LogisticRegression``)
-        The type of classifier to be used to make the predictions.
-    model_kwargs : dict (default=``{}``)
-        Keyword arguments for ``model_class``. 
-    parameter_search_grid : ``None`` or dict (default=``None``)
-        If not ``None``, then ``sklearn.grid_search.GridSearchCV`` will be
-        used and this parameter should contain a dictionary with the ``param_grid``
-        positional argument for that estimator. See note below about naming conventions,
-        which are slightly nonstandard.
+    copy_data : boolean (default=``True``)
+        Whether or not to copy data when fitting and applying the model. Running the model
+        in-place (``copy_data=False``) will be faster and have a smaller memory footprint,
+        but if not done carefully can lead to data integrity issues.
 
+    Attributes
+    ----------
+    model : A Scikit-learn pipeline (or equivalent)
+        The actual model used to compute WP. Upon initialization it will be set to
+        a default model, but can be overridden by the user.
 
-    Notes
-    -----
-    When using the ``parameter_search_grid`` parameter, in order to adjust the hyperparameters
-    of the selected ``model_class`` you'll need to prepend them with ``base_estimator``. For
-    instance, in the case of ``sklearn.linear_model.LogisticRegression`` you could have
-    ``{'base_estimator__penalty': ['l1', 'l2'], 'base_estimator__C': [0.1, 1, 10]}``. This is
-    necessary because ``WPModel`` wraps the ``model_class`` with an instances of
-    ``sklearn.calibration.CalibratedClassifierCV`` to attempt to provide a better fit to the
-    probabilities, rather than just optimizing on predictive accuracy. If you want to run
-    cross-validation on the ``CalibratedClassifierCV`` hyperparameters you can: those you **don't**
-    need to preface with ``base_estimator__``. 
     """
 
     def __init__(self,
@@ -76,11 +62,9 @@ class WPModel(object):
                  yards_to_go_colname="yards_to_go",
                  yardline_colname="yardline",
                  offense_team_colname="offense_team",
-                 home_team_colname = "home_team",
-                 copy=True,
-                 model_class=LogisticRegression,
-                 model_kwargs={},
-                 parameter_search_grid=None):
+                 home_team_colname="home_team",
+                 copy_data=True
+                ):
         self.home_score_colname = home_score_colname
         self.away_score_colname = away_score_colname
         self.quarter_colname = quarter_colname
@@ -90,39 +74,31 @@ class WPModel(object):
         self.yardline_colname = yardline_colname
         self.offense_team_colname = offense_team_colname
         self.home_team_colname = home_team_colname
-        self.copy = copy
-        self.model_class = model_class
-        self.model_kwargs = model_kwargs
-        self.parameter_search_grid = parameter_search_grid
+        self.copy_data = copy_data
 
-        self.model = self.create_pipeline()
-
-        self._fit = False
+        self.model = None
 
 
-    def create_pipeline(self):
-        """Create the win probability estimation pipeline.
+    def create_default_pipeline(self):
+        """Create the default win probability estimation pipeline.
 
-
-        Returns
-        -------
-        sklearn.pipeline.Pipeline
-            A pipeline object containing all the steps in the model.
+        This sets the ``model`` attribute of the class, so be aware that
+        if you run it it will overwrite any model you have stored already.
         """
 
         steps = []
 
         is_offense_home = preprocessing.ComputeIfOffenseIsHome(self.offense_team_colname,
                                                                self.home_team_colname,
-                                                               copy=self.copy)
+                                                               copy=self.copy_data)
         steps.append(("compute_offense_home", is_offense_home))
         score_differential = preprocessing.CreateScoreDifferential(self.home_score_colname,
                                                                    self.away_score_colname,
                                                                    is_offense_home.offense_home_team_colname,
-                                                                   copy=self.copy)
+                                                                   copy=self.copy_data)
         steps.append(("create_score_differential", score_differential))
-        steps.append(("map_downs_to_int", preprocessing.MapToInt(self.down_colname, copy=self.copy)))
-        total_time_elapsed = preprocessing.ComputeElapsedTime(self.quarter_colname, self.time_colname, copy=self.copy)
+        steps.append(("map_downs_to_int", preprocessing.MapToInt(self.down_colname, copy=self.copy_data)))
+        total_time_elapsed = preprocessing.ComputeElapsedTime(self.quarter_colname, self.time_colname, copy=self.copy_data)
         steps.append(("compute_total_time_elapsed", total_time_elapsed))
         steps.append(("remove_unnecessary_columns", preprocessing.CheckColumnNames(
             column_names=[is_offense_home.offense_home_team_colname,
@@ -131,10 +107,10 @@ class WPModel(object):
                           self.yardline_colname,
                           self.yards_to_go_colname,
                           self.down_colname],
-            copy=self.copy)))
+            copy=self.copy_data)))
         steps.append(("encode_categorical_columns", preprocessing.OneHotEncoderFromDataFrame(
             categorical_feature_names=[self.down_colname],
-            copy=self.copy)))
+            copy=self.copy_data)))
 
         model = self.model_class(**self.model_kwargs)
         model = CalibratedClassifierCV(model, cv=2, method="isotonic")
@@ -146,7 +122,7 @@ class WPModel(object):
         steps.append(("compute_model", model))
 
         pipe = Pipeline(steps)
-        return pipe
+        self.model = pipe
 
     @staticmethod
     def _brier_loss_scorer(estimator, X, y):
@@ -157,65 +133,11 @@ class WPModel(object):
         predicted_positive_probabilities = estimator.predict_proba(X)[:, 1]
         return 1. - brier_score_loss(y, predicted_positive_probabilities)
 
-    def fit(self, X, y):
-        """Fit the model to data.
-
-        Parameters
-        ----------
-        X : Pandas DataFrame, of shape (n_plays, n_features)
-            The plays to be used to fit the model.
-        y : numpy array or Pandas Series, of length n_plays
-            The result of the game (offense win/lose) for each play.
-
-        Returns
-        -------
-        self
-            Preserving the Scikit-learn idiom.
-        """
-        
-        self.model.fit(X, y)
-        self._fit = True
-        return self
-
-    def get_model_params(self):
-        """Return the hyperparameters used in the fitted model.
-
-        This wrapper is necessary because the API changes if you
-        use ``GridSearchCV`` instead of a bare classifier.
-
-        Returns
-        -------
-        dict
-            The parameter names and their values.
-
-        Raises
-        ------
-        NotFittedError
-            If ``fit`` has not been called yet.
-
-        Notes
-        -----
-        Because the classifier is wrapped to force better probability estimates,
-        the parameters of the classifier will be prepended with "best_estimator__".
-        """
-        if not self._fit:
-            raise NotFittedError("WPModel.get_model_params: Must run 'fit' first!")
-
-        last_step = self.model.steps[-1][1]
-
-        try:
-            best_hyperparams_dict = last_step.best_params_
-            params_dict = last_step.estimator.get_params()
-            for param in best_hyperparams_dict:
-                params_dict[param] = best_hyperparams_dict[param]
-            return params_dict
-        except AttributeError:
-            return last_step.get_params()
 
 if __name__ == "__main__":
     import time
     start = time.time()
-    play_df = utilities.get_nfldb_play_data(season_years=[2013, 2014])
+    play_df = utilities.get_nfldb_play_data(season_years=[2009, 2010, 2011, 2012, 2013, 2014])
     target_col = play_df["offense_won"]
     play_df.drop("offense_won", axis=1, inplace=True)
     play_df_train, play_df_test, target_col_train, target_col_test = (
@@ -223,14 +145,14 @@ if __name__ == "__main__":
     
     print("Took {0:.2f}s to query data".format(time.time() - start))
     start = time.time()
-    #win_probability_model = WPModel()
-    # win_probability_model = WPModel(model_class=LogisticRegression,
-    #                                 parameter_search_grid={'base_estimator__penalty': ['l1', 'l2'],
-    #                                                        'base_estimator__C': [0.1, 1, 10],
+    # win_probability_model = WPModel()
+    win_probability_model = WPModel(model_class=LogisticRegression,
+                                    parameter_search_grid={'base_estimator__penalty': ['l1', 'l2'],
+                                                           'base_estimator__C': [0.01, 0.1, 1, 10, 100],
+                                                           'method': ['isotonic', 'sigmoid']})
+    # win_probability_model = WPModel(model_class=RandomForestClassifier,
+    #                                 parameter_search_grid={'base_estimator__n_estimators': [10, 50, 100, 150, 200],
     #                                                        'method': ['isotonic']})
-    win_probability_model = WPModel(model_class=RandomForestClassifier,
-                                    parameter_search_grid={'base_estimator__n_estimators': [5, 40],
-                                                           'method': ['isotonic']})
     pipe = win_probability_model.model
     print("Took {0:.2f}s to create pipeline".format(time.time() - start))
 
@@ -241,22 +163,31 @@ if __name__ == "__main__":
 
     predicted_win_probabilities = pipe.predict_proba(play_df_test)[:,1]
 
+
     kde_offense_won = KernelDensity(kernel='gaussian', bandwidth=0.01).fit(
         (predicted_win_probabilities[(target_col_test.values == 1)])[:, np.newaxis])
     kde_total = KernelDensity(kernel='gaussian', bandwidth=0.01).fit(
         predicted_win_probabilities[:, np.newaxis])
     sample_probabilities = np.linspace(0, 1, 101)[:, np.newaxis]
-    number_offense_won = np.exp(kde_offense_won.score_samples(sample_probabilities)) * np.sum((target_col_test))
-    number_total = np.exp(kde_total.score_samples(sample_probabilities)) * len(target_col_test) #actually number density
+    number_density_offense_won = np.exp(kde_offense_won.score_samples(sample_probabilities)) * np.sum((target_col_test))
+    number_density_total = np.exp(kde_total.score_samples(sample_probabilities)) * len(target_col_test)
+    number_offense_won = number_density_offense_won * np.sum(target_col_test) / np.sum(number_density_offense_won)
+    number_total = number_density_total * len(target_col_test) / np.sum(number_density_total)
     predicted_win_percents = number_offense_won / number_total
+    from statsmodels.stats.proportion import proportion_confint
+    win_pct_errors = np.array([proportion_confint(sample_probabilities[i,0]*number_total[i], number_total[i], method="jeffrey", alpha=0.333) for i in range(len(number_total))])
     max_deviation = np.max(np.abs(predicted_win_percents - sample_probabilities[:, 0]))
     print("Max deviation: {0:.2f}%".format(max_deviation * 100))
-    print("DEBUG: ", len(target_col_test))
     
     import matplotlib.pyplot as plt
+    plt.style.use('ggplot')
     ax = plt.figure().add_subplot(111)
     ax.plot([0, 1], [0, 1], ls="--", lw=2, color="black")
-    ax.plot(sample_probabilities[:, 0], predicted_win_percents, ls="-", lw=3, color="green",
+    ax.fill_between(sample_probabilities[:, 0],
+                    win_pct_errors[:,0],
+                    win_pct_errors[:,1],
+                    facecolor="blue", alpha=0.25)
+    ax.plot(sample_probabilities[:, 0], predicted_win_percents, ls="-", color="blue",
             label="Max Deviation = {0:.2f}%".format(max_deviation * 100))
     ax.set_xlabel("Predicted WP")
     ax.set_ylabel("Actual WP")
@@ -264,7 +195,7 @@ if __name__ == "__main__":
     ax2 = ax.twinx()
     ax2.fill_between(sample_probabilities[:, 0], number_total,
                      facecolor="gray", alpha=0.25, interpolate=True)
-    ax2.set_ylabel("Number of Plays")
+    ax2.set_ylabel("Number of Plays in Test Set")
     # ax.fill_between(sample_probabilities[:,0], number_total,
     #                 facecolor="blue", alpha=0.25, interpolate=True, label="all_probabilities")
     # ax.fill_between(sample_probabilities[:,0], number_offense_won,
