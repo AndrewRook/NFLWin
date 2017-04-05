e5cc0667795e48bec857878fbec189416ce35f04
Andrew Schechtman-Rook <rook166@gmail.com>
2017-04-04 22:11:21 -0400

    got game query working in sqlalchemy

--- nflwin/data.py
+++ nflwin/data.py
@@ -3,8 +3,10 @@ try:
 except ImportError:
     from configparser import RawConfigParser
 import os
-import sqlalchemy as sql
 import sys
+
+import pandas as pd
+import sqlalchemy as sa
 
 def connect_nfldb(config_path=''):
     """Connect to an existing nfldb database.
@@ -48,7 +50,7 @@ def connect_nfldb(config_path=''):
                     "port": cp.getint("pgsql", "port")
                 }
 
-                engine = sql.create_engine(sql.engine.url.URL(**db_config))
+                engine = sa.create_engine(sa.engine.url.URL(**db_config))
     
                 return engine
         except IOError:
@@ -58,89 +60,88 @@ def connect_nfldb(config_path=''):
     raise IOError("connect_nfldb: couldn't find a configuration file. "
                   "looked here: {0}".format(", ".join(paths)))
 
+def _aggregate_wins(game_table):
+    subquery_columns = [game_table.c.home_team,
+                        game_table.c.away_team,
+                        game_table.c.season_year]
+    subquery_columns.append(
+        (sa.case(
+            [
+             (game_table.c.season_type == "Regular", 0)
+            ],
+            else_=1) * 100 + game_table.c.week).label("game_order"))
+    subquery_columns.append(
+        (game_table.c.home_score > game_table.c.away_score).label("home_win"))
+    subquery = (sa.sql.select(subquery_columns)
+                .where(game_table.c.season_type != "Preseason")
+                .alias("subquery"))
+
+    query_columns = [sa.func.max(game_table.c.gsis_id).label("gsis_id"),
+                     sa.func.max(game_table.c.season_year).label("season_year"),
+                     sa.func.max(game_table.c.season_type).label("season_type"),
+                     sa.func.max(game_table.c.week).label("week")]
+    query_columns.append(
+        sa.func.sum(sa.case(
+            [(sa.and_(game_table.c.home_team == subquery.c.home_team,
+                      subquery.c.home_win == True), 1),
+             (sa.and_(game_table.c.home_team == subquery.c.away_team,
+                      subquery.c.home_win == False), 1)
+            ], else_=0)).label("home_wins"))
+    query_columns.append(
+        sa.func.sum(sa.case(
+            [(sa.and_(game_table.c.home_team == subquery.c.home_team,
+                      subquery.c.home_win == False), 1),
+             (sa.and_(game_table.c.home_team == subquery.c.away_team,
+                      subquery.c.home_win == True), 1)
+            ], else_=0)).label("home_losses"))
+    query_columns.append(
+        sa.func.sum(sa.case(
+            [(sa.and_(game_table.c.away_team == subquery.c.home_team,
+                      subquery.c.home_win == True), 1),
+             (sa.and_(game_table.c.away_team == subquery.c.away_team,
+                      subquery.c.home_win == False), 1)
+            ], else_=0)).label("away_wins"))
+    query_columns.append(
+        sa.func.sum(sa.case(
+            [(sa.and_(game_table.c.away_team == subquery.c.home_team,
+                      subquery.c.home_win == False), 1),
+             (sa.and_(game_table.c.away_team == subquery.c.away_team,
+                      subquery.c.home_win == True), 1)
+            ], else_=0)).label("away_losses"))
+
+    query = (sa.sql.select(query_columns)
+             .select_from(game_table.join(subquery, sa.and_(
+                 game_table.c.season_year == subquery.c.season_year,
+                 sa.case(
+                     [
+                         (game_table.c.season_type == "Regular", 0)
+                     ],
+                     else_=1) * 100 + game_table.c.week > subquery.c.game_order,
+                 sa.or_(
+                     game_table.c.home_team == subquery.c.home_team,
+                     game_table.c.home_team == subquery.c.away_team,
+                     game_table.c.away_team == subquery.c.home_team,
+                     game_table.c.away_team == subquery.c.away_team))))
+             .where(game_table.c.season_type != "Preseason")
+             .group_by(game_table.c.gsis_id))
+                      
+                     
+    return query
+
 def query_nfldb(engine, season_years=None, season_types=["Regular", "Postseason"]):
     """"""
-    metadata = sql.MetaData(engine)
-    #team = sql.Table("team", metadata, autoload=True)
-    play = sql.Table("play", metadata,
-                     #sql.Column("time", GameTime()),
-                     autoload=True)
-    #agg_play = sql.Table("agg_play", metadata, autoload=True)
-    #game = sql.Table("game", metadata, autoload=True)
+    metadata = sa.MetaData(engine)
+    tables = {}
+    tables["team"] = sa.Table("team", metadata, autoload=True)
+    tables["play"] = sa.Table("play", metadata, autoload=True)
+    tables["agg_play"] = sa.Table("agg_play", metadata, autoload=True)
+    tables["game"] = sa.Table("game", metadata, autoload=True)
 
-    print(dir(play.c.time))
-    select = sql.sql.select([play.c.time]).limit(10)
-    conn = engine.connect()
-    import pandas as pd
-    df = pd.read_sql(select, conn)
-    #print(df)
-    #result = conn.execute(select)
-    #for row in result:
-    #    print(row)
-    # for column in sorted([c.name for c in play.columns]):
-    #     print(column)
-    #print(agg_play.columns)
+    test = _aggregate_wins(tables["game"])
+    with engine.connect() as conn:
+        df = pd.read_sql(test.limit(20), conn)
 
-# class GameTime(sql.types.TypeDecorator):
-#     impl = sql.types.Unicode
-#     def process_bind_param(self, value, dialect):
-#         return "({0},{1})".format(value[0], value[1])
-#     def process_result_value(self, value, dialect):
-#         values = value[1:-1].split(",")
-#         values[1] = int(values[1])
-#         return values
-            
-
-# class GameTime(sql.types.UserDefinedType):
-#     name = "game_time"
-
-#     def __init__(self):
-#         pass
-#     def get_col_spec(self):
-#         return "{0}(quarter, seconds_elapsed)".format(name)
-
-#     def result_processor(self, dialect, coltype):
-#         def process(value):
-#             values = value[1:-1].split(",")
-#             values[1] = int(values[1])
-#             return values
-#         return process
-    
-#     def bind_processor(self, dialect):
-#         def process(value):
-#             return "({0},{1})".format(value[0], value[1])
-#         return process
-#     def test(self, dialect, coltype):
-#         def process(value):
-#             return value[1:-1].split(",")
-#         return process
-
-# import re
-# import psycopg2
-# from psycopg2.extensions import adapt, register_adapter, AsIs
-# class GameTime(object):
-#     def __init__(self, quarter, seconds_elapsed):
-#         self.quarter = quarter
-#         self.seconds_elapsed = seconds_elapsed
-
-# def adapt_game_time(game_time):
-#     return AsIs("'({0},{1})'".format(adapt(game_time.quarter), adapt(game_time.seconds_elapsed)))
-
-# def cast_game_time(value, cur):
-#     if value is None:
-#         return None
-#     m = re.match(r"\(([^)]+),([^)]+)\)", value)
-#     if m:
-#         return GameTime(m.group(1), int(m.group(2)))
-#     else:
-#         raise psycopg2.InterfaceError("bad point representation: %r" % value)
-
-# def register_game_time_type(engine):
-#     register_adapter(GameTime, adapt_game_time)
-#     rs = engine.execute("SELECT NULL::game_time")
-#     game_time_old = rs.cursor.description[0][1]
-#     GAME_TIME = psycopg2.extensions.new_type((game_time_old,), "game_time", cast_game_time)
-#     psycopg2.extensions.register_type(GAME_TIME)
+    print(df)
 
 
 if __name__ == "__main__":
